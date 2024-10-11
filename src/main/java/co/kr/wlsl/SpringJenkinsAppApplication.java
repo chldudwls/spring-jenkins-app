8cbe225fe4dcf5c894b2a53dfbbdf2ee583b2ad7
lotte4 <dudwls4251@gamil.com>
2024-10-11 11:26:25 +0900

    first commit

---
+++ src/main/java/co/kr/wlsl/SpringJenkinsAppApplication.java
@@ -0,0 +1,13 @@
+package co.kr.wlsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsAppApplication.class, args);
+    }
+
+}
